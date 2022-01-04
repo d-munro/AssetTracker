7349e315368a0824a28b57f9914ab5e36f3d313e
Dylan Munro <dylmunro@gmail.com>
2022-01-04 07:16:36 -0500

    Implements skeleton methods and documentation

--- AssetTracker.py
+++ AssetTracker.py
@@ -16,7 +16,6 @@ class variables:
     name - The full name of the asset (Ex: Apple, Bitcoin)
     ticker - The ticker for the asset (Ex: AAPL, BTC)
     type - Declares if a ticker is for a cryptocurrency or a stock
-    file - The file which stores asset data if not using coingecko API
     
 """
 class Asset:
@@ -24,7 +23,10 @@ class Asset:
     def __init__(self):
         pass
     
-    def get_price(self, ticker: str, date: datetime.datetime()) -> float:
+    """
+    Returns the price of the asset at a specified date
+    """
+    def get_price(self, date: datetime.datetime()) -> float:
         pass
     
 #get_price(ticker, datetime.datetime(year, month, day, hour, minute, second))
@@ -32,7 +34,7 @@ class Asset:
 """
 Contains all methods for plotting graphs of the specified ticker
 
-Params:
+class variables:
     ticker: The ticker of the stock/cryptocurrency being plotted
     
 """
@@ -48,17 +50,57 @@ class Graph:
     """
     Plots the last 50 days of the closing price for the specified asset
     """
-    def plot_asset(self, asset, final_date=datetime.today()):
+    def plot_asset(self, final_date=datetime.today()):
         pass
 
     """
-    Plots the 20 day SMA for the asset
+    Plots the 20 day SMA for an asset
     """
-    def plot_20_day_sma(self, asset):
+    def plot_20_day_sma(self):
+        pass
+    
+    """
+    Plots the 20 day EMA for an asset
+    """
+    def plot_20_day_ema(self):
+        pass
+    
+    """
+    Plots the 50 day SMA for an asset
+    """
+    def plot_50_day_sma(self):
+        pass
+    
+    """
+    Plots the 50 day EMA for an asset
+    """
+    def plot_50_day_ema(self):
         pass
 
 """
 Handles all IO requests from the user
+
+class variables:
+    file - The file which stores asset data if not using coingecko API
 """
 class IO:
-    pass
+    
+    def load(self):
+        pass
+    
+    """
+    The main method which handles the program control flow
+    """
+    def main(self):
+        self.load()
+        self.run()
+    
+    """
+    Parses given csv file for an asset to obtain all relevant information
+    """
+    def parse_file(self, file_name):
+        pass
+    
+    def run(self):
+        pass
+    
